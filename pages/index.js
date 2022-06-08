9f870a723da3f3732389ebcf87b4a8a3a0def15e
Cooper Saye <coopersaye@gmail.com>
2022-06-07 18:03:03 -0700

    Create index page content.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,12 @@ import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.center}>
+        <h1>CodeWithGames</h1>
+        <p>Learn to code by making games.</p>
+        <p><i>Coming soon.</i></p>
+      </div>
     </div>
   );
 }
