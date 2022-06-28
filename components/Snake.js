5e76867a355130eca89fd652cd3071b82e87f569
Cooper Saye <coopersaye@gmail.com>
2022-06-28 15:14:14 -0700

    Get canvas context on start.

--- components/Snake.js
+++ components/Snake.js
@@ -1,8 +1,23 @@
+import { useEffect, useRef, useState } from 'react';
+
 import styles from '../styles/components/Snake.module.css';
 
+let canvas, ctx;
+
 export default function Snake() {
+  const canvasRef = useRef();
+
+  // on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div>
+      <canvas
+        ref={canvasRef}
+      />
     </div>
   );
 }
