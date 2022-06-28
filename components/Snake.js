ef86921942c0856e6a4fadb8bd81629c1fa0199b
Cooper Saye <coopersaye@gmail.com>
2022-06-28 15:15:38 -0700

    Set up canvas resize listener.

--- components/Snake.js
+++ components/Snake.js
@@ -7,16 +7,36 @@ let canvas, ctx;
 export default function Snake() {
   const canvasRef = useRef();
 
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  // called on window resize
+  function onResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
   // on start
   useEffect(() => {
     canvas = canvasRef.current;
     ctx = canvas.getContext('2d');
+    // initialize dimensions
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+    // set up resize listener
+    window.addEventListener('resize', onResize);
+    // clean up listeners on return
+    return () => {
+      window.removeEventListener('resize', onResize);
+    }
   }, []);
 
   return (
     <div>
       <canvas
         ref={canvasRef}
+        width={width}
+        height={height}
       />
     </div>
   );
