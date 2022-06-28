bfae4b1bf779c14e55b7ef2431a0a31dae20a41d
Cooper Saye <coopersaye@gmail.com>
2022-06-28 15:17:48 -0700

    Create canvas draw function.

--- components/Snake.js
+++ components/Snake.js
@@ -2,13 +2,26 @@ import { useEffect, useRef, useState } from 'react';
 
 import styles from '../styles/components/Snake.module.css';
 
+const mapSize = 16; // map grid size
+const borderPixels = 1; // pixels in grid border
+
 let canvas, ctx;
+let snakeX, snakeY;
+let appleX, appleY;
+let snake = [];
 
 export default function Snake() {
   const canvasRef = useRef();
 
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
+
+  // draws canvas
+  function draw() {
+    // clear canvas
+    ctx.fillStyle = '#eee';
+    ctx.fillRect(0, 0, width, height);
+  }
 
   // called on window resize
   function onResize() {
