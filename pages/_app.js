8063a20e2764de488c5a4f8186ec26a04a8a91cb
Cooper Saye <coopersaye@gmail.com>
2022-06-28 15:09:32 -0700

    Move link from _app.

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,6 @@ export default function App(props) {
         <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
