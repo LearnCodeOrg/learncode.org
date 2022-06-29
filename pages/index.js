23632755302245dc78429224eaa495590f727f1b
Cooper Saye <coopersaye@gmail.com>
2022-06-29 13:21:01 -0700

    Add icons to index page.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import Snake from '../components/Snake';
+import Link from '../components/Link';
+
+import { useState } from 'react';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
@@ -24,6 +32,17 @@ export default function Index() {
           }
         </p>
         <span>coming soon</span>
+        <div className={styles.icons}>
+          <Link to="https://github.com/codewithgames">
+            <GitHubIcon />
+          </Link>
+          <Link to="mailto:hi@codewith.games">
+            <EmailIcon />
+          </Link>
+          <Link to="https://twitter.com/CodeWGames">
+            <TwitterIcon />
+          </Link>
+        </div>
       </div>
     </div>
   );
