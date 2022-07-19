c51c883043951bd0214905ac129f381d081d70fe
Cooper Saye <coopersaye@gmail.com>
2022-07-18 22:29:14 -0700

    Construct domains page layout.

--- pages/domains.js
+++ pages/domains.js
@@ -6,6 +6,20 @@ export default function Domains() {
       <div className={styles.background}>
         <div />
       </div>
+      <div className={styles.content}>
+        <div className={styles.center}>
+          <h1>LearnCode.org Domains</h1>
+          <div className={styles.listings}>
+          </div>
+          <p id="questions"><b>Questions?</b></p>
+          <a
+            href="mailto:hi@learncode.org?subject=Domain Inquiry"
+            className={styles.contactUs}
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
