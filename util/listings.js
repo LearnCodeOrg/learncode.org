dc66f9dcb82c4c6bedec6d45567c1207591352ed
Cooper Saye <coopersaye@gmail.com>
2022-07-18 18:35:00 -0700

    Define domain listings.

---
+++ util/listings.js
@@ -0,0 +1,90 @@
+export const listings = [
+  {
+    domain: 'Bicycle.social',
+    description: 'geo-based social network'
+  },
+  {
+    domain: 'Brainwave.team',
+    description: 'group idea development'
+  },
+  {
+    domain: 'BrainyCoding.com',
+    description: 'code tutoring service'
+  },
+  {
+    domain: 'ChampionCoding.com',
+    description: 'code tutoring service'
+  },
+  {
+    domain: 'CodeChampion.io',
+    description: 'code learning platform'
+  },
+  {
+    domain: 'CodeChampion.net',
+    description: 'code learning platform'
+  },
+  {
+    domain: 'CodeChampion.org',
+    description: 'code learning platform'
+  },
+  {
+    domain: 'CodersCreate.com',
+    description: 'coder collaboration forum'
+  },
+  {
+    domain: 'CodeTeach.org',
+    description: 'code tutoring service'
+  },
+  {
+    domain: 'CodeTeacher.org',
+    description: 'code tutoring service'
+  },
+  {
+    domain: 'CodeWG.com',
+    description: 'code with games'
+  },
+  {
+    domain: 'CodeWith.games',
+    description: 'code with games'
+  },
+  {
+    domain: 'CometCoding.com',
+    description: 'code tutoring service'
+  },
+  {
+    domain: 'DevComms.io',
+    description: 'dev communications app'
+  },
+  {
+    domain: 'Earlies.net',
+    description: 'early learning app for kids'
+  },
+  {
+    domain: 'Fields.fun',
+    description: 'field-themed online game'
+  },
+  {
+    domain: 'GiftedCoding.com',
+    description: 'code tutoring service'
+  },
+  {
+    domain: 'InFive.org',
+    description: 'five-minute online lessons'
+  },
+  {
+    domain: 'TalentCoding.com',
+    description: 'code tutoring service'
+  },
+  {
+    domain: 'TalentedCoding.com',
+    description: 'code tutoring service'
+  },
+  {
+    domain: 'WickBridge.com',
+    description: 'online candle shop'
+  },
+  {
+    domain: 'WickFaces.com',
+    description: 'candle social network'
+  }
+];
