415969ee9d853bfa50e9646a2d5e583d935a9972
Cooper Saye <coopersaye@gmail.com>
2022-07-18 22:31:04 -0700

    Map domain listings.

--- pages/domains.js
+++ pages/domains.js
@@ -1,4 +1,6 @@
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import styles from '../styles/pages/Domains.module.css';
+import { listings } from '../util/listings';
 
 export default function Domains() {
   return (
@@ -10,6 +12,33 @@ export default function Domains() {
         <div className={styles.center}>
           <h1>LearnCode.org Domains</h1>
           <div className={styles.listings}>
+            {
+              listings.map((listing, i) =>
+                <div
+                  id={listing.domain.toLowerCase()}
+                  className={styles.listing}
+                  key={i}
+                >
+                  <div className={styles.details}>
+                    <a
+                      href={`https://www.namecheap.com/market/buynow/${listing.domain.toLowerCase()}/`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {listing.domain}
+                    </a>
+                    <p>{listing.description}</p>
+                  </div>
+                  <a
+                    href={`https://www.namecheap.com/market/buynow/${listing.domain.toLowerCase()}/`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <AddShoppingCartIcon />Buy Now
+                  </a>
+                </div>
+              )
+            }
           </div>
           <p id="questions"><b>Questions?</b></p>
           <a
